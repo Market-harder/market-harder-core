bce0f1abc694b63b25920784790da1d2a614e86e
Augustine Osagie <osagie98@gmail.com>
2021-10-10 09:09:55 -0700

    Refined ip script

--- ip-address.js
+++ ip-address.js
@@ -1,3 +1,23 @@
-function printIP(ip) {
-    document.write(ip);
+
+async function sendIP(ipAddr) {
+    url = 'https://j1e82lg769.execute-api.us-east-2.amazonaws.com/test/searchIp';
+    fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'text/plain',
+            'Accept': '*/*'
+        },
+        body: JSON.stringify({ ip : ipAddr})
+    })
+    .then(data => {
+        console.log('data');
+        console.log(data);
+        return data;
+    })
+    .catch(error => {
+        console.log('error');
+        console.log(error);
+        return error;
+    });
 }
